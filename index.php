090b9b8ec76492ee8335d2c934c1acf036129dfd
tsujita <tsujita.shou@gmail.com>
2013-02-20 00:47:06 +0900

    UrlGeneratorServiceProviderを導入

--- index.php
+++ index.php
@@ -3,15 +3,25 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
+
+$app->get('/', function () {
+    return 'welcome to the homepage';
+})
+->bind('homepage');
 
 $app->get('/hello/{name}', function($name) use($app) { 
     return $app['twig']->render('hello.twig', array(
         'name' => $name,
     ));
-}); 
+})
+->bind('hello'); 
 
 $app->run(); 
 
